c0ac968c9713222c044fa41cd7c799d10e2b6f9e
changanuong1742 <changanuong1742@gmail.com>
2019-10-31 10:03:20 +0700

    ver1

---
+++ JavaHIenThiDate.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class JavaHIenThiDate {
+
+    public static void main(String[] args) {
+
+
+        Date now = new Date();
+
+        System.out.println("Now is: " + now);
+
+    }
+}
